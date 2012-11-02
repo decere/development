80b35a3b777ed8afc0b1c6f9151c0352cef91a6c
Matt Moohyun Shin <matt.shin@gmail.com>
2012-11-03 00:11:36 +0900

    improved the way symbolic link is created so that it always works

--- conf/make.symlinks.sh
+++ conf/make.symlinks.sh
@@ -4,16 +4,16 @@ DATA_DIR=/data/matt
 DEVELOPMENT_DIR=${DATA_DIR}/development
 CONF_DIR=${DEVELOPMENT_DIR}/conf
 
-ln -s ${CONF_DIR}/vim ~/.vim
-ln -s ${CONF_DIR}/vimrc ~/.vimrc
-ln -s ${CONF_DIR}/vimrc.etc ~/.vimrc.etc
-ln -s ${CONF_DIR}/bash_aliases ~/.bash_aliases
+ln -sfn ${CONF_DIR}/vim ~/.vim
+ln -sfn ${CONF_DIR}/vimrc ~/.vimrc
+ln -sfn ${CONF_DIR}/vimrc.etc ~/.vimrc.etc
+ln -sfn ${CONF_DIR}/bash_aliases ~/.bash_aliases
 
-ln -s ${DEVELOPMENT_DIR}/bin ~/bin 
-ln -s ${DATA_DIR} ~/data
-ln -s ${DATA_DIR}/src ~/src
-ln -s ${DATA_DIR}/docs ~/docs
-ln -s ${DATA_DIR}/download ~/download
-ln -s ${DATA_DIR}/android-sdk-data ~/.android
+ln -sfn ${DEVELOPMENT_DIR}/bin ~/bin 
+ln -sfn ${DATA_DIR} ~/data
+ln -sfn ${DATA_DIR}/src ~/src
+ln -sfn ${DATA_DIR}/docs ~/docs
+ln -sfn ${DATA_DIR}/download ~/download
+ln -sfn ${DATA_DIR}/android-sdk-data ~/.android
 
 mkdir ~/.vimback
