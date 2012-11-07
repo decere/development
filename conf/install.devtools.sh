03e415ad994c4c04907e1d8077269109fb05aa1b
Matt Moohyun Shin <matt.shin@gmail.com>
2012-11-08 00:09:02 +0900

    configuring vim as a git editor and meld as a merge tool

--- conf/install.devtools.sh
+++ conf/install.devtools.sh
@@ -1,5 +1,6 @@
-sudo apt-get install vim ctags cscope git-core global g++
+sudo apt-get install vim ctags cscope git-core global g++ meld
 git config --global user.name "Matt Moohyun Shin"
 git config --global user.email matt.shin@gmail.com
 git config --global color.ui true
-
+git config --global core.editor vim
+git config --global merge.tool meld
